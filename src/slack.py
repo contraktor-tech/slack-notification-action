e53fa2b82fa30896f46a98bf0ee610b694651313
robsonandradev <robsom.s.a@gmail.com>
2022-03-18 16:45:45 -0300

    Replace if condition

--- src/slack.py
+++ src/slack.py
@@ -5,11 +5,11 @@ environment = os.environ['ENVIRONMENT_NAME']
 status = os.environ['DEPLOY_STATUS']
 
 def send_message(project: str, environment: str, status: str) -> bool:
-  if status == 'success':
-    user = "Spacex"
-    msg = f"Deploy do projeto '{ project }' do ambiente '{ environment }' concluído com sucesso.   :rocket:"
-    icon_url = "https://ck-devops.s3.amazonaws.com/rocket-icon.png"
-  else:
+  user = "Spacex"
+  msg = f"Deploy do projeto '{ project }' do ambiente '{ environment }' concluído com sucesso.   :rocket:"
+  icon_url = "https://ck-devops.s3.amazonaws.com/rocket-icon.png"
+
+  if status == 'failure':
     user = "Houston, we have a problem!"
     msg = f"Tem que ver isso ae, talkei. Projeto '{ project }' do ambiente '{ environment }' falhou.  :boom:"
     icon_url = "https://ck-devops.s3.amazonaws.com/deploy-fail-icon.jpg"
