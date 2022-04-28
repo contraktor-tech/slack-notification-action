a1cac4f6fc6e2473802d6eb68fe854b077aa2049
robsonandradev <robsom.s.a@gmail.com>
2022-04-28 10:36:45 -0300

    Replace message, insert argo cd link

--- src/slack.py
+++ src/slack.py
@@ -1,17 +1,28 @@
 import json, os, requests
 
+argocd_domain = os.environ['ARGOCD_DOMAIN']
+argocd_app = os.environ['ARGOCD_APP']
 project = os.environ['PROJECT_NAME']
 environment = os.environ['ENVIRONMENT_NAME'] 
 status = os.environ['DEPLOY_STATUS']
 
-def send_message(project: str, environment: str, status: str) -> bool:
+def send_message(argocd_domain: str, argocd_app: str, project: str, environment: str, status: str) -> bool:
   user = "SpaceX"
-  msg = f"Deploy do *{ project }* no ambiente *{ environment }* concluído com sucesso.   :rocket:"
+  argocd_link = f'https://{ argocd_domain }/applications/{ argocd_app }?resource='
+  msg = f""" 
+          O Argo CD está tentando fazer deploy do *{ project }* no ambiente *{ environment }*. :rocket:
+          Acompanhe a tentativa de deploy em *{ argocd_link }*.
+        """
+
   icon_url = "https://ck-devops.s3.amazonaws.com/deploy-success-icon.png"
 
   if status == 'failure':
     user = "Houston, we have a problem!"
-    msg = f"Tem que ver isso ae, talkei. Deploy do *{ project }* no ambiente *{ environment }* falhou.  :boom:"
+    msg = f"""
+            O Argo CD falhou na tentativa de deploy do *{ project }* no ambiente *{ environment }*.  :boom:
+            Verifique o erro em *{ argocd_link }*
+          """
+
     icon_url = "https://ck-devops.s3.amazonaws.com/deploy-fail-icon.png"
 
   url = os.environ['SLACK_HOOK']
@@ -23,7 +34,8 @@ def send_message(project: str, environment: str, status: str) -> bool:
       "username": user,
       "text": msg,
       "icon_url": icon_url,
-      "channel": "C01UT9Q6NJ0"
+      # "channel": "C01UT9Q6NJ0"
+      "channel": "C037Q3ZEYPM"
   })
 
   response = requests.post(url, data=data)
@@ -33,4 +45,4 @@ def send_message(project: str, environment: str, status: str) -> bool:
   
   return False
 
-send_message(project, environment, status)
+send_message(argocd_domain, argocd_app, project, environment, status)
